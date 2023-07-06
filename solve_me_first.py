f64caae3bb68921b3cefa5f261cbc1a082836424
Bharathi-raja-pbr <76565518+Bharathi-raja-pbr@users.noreply.github.com>
2023-07-06 10:56:36 +0530

    Create solve_me_first.py
    Function Description
    
    Complete the solveMeFirst function in the editor below.
    
    solveMeFirst has the following parameters:
    
    int a: the first value
    int b: the second value
    Returns
    - int: the sum of  a and b

---
+++ solve_me_first.py
@@ -0,0 +1,18 @@
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
+
+'''
+Function Description
+
+Complete the solveMeFirst function in the editor below.
+
+solveMeFirst has the following parameters:
+
+int a: the first value
+int b: the second value
+Returns
+- int: the sum of a and b '''

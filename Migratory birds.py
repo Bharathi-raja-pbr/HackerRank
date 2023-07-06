cf433c5646147bb784ad2eb0936504ae720d435d
Bharathi-raja-pbr <76565518+Bharathi-raja-pbr@users.noreply.github.com>
2023-07-06 11:17:28 +0530

    Migratory birds.py

---
+++ Migratory birds.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    # Write your code here
+    u=[]
+    for x in arr:
+        if x not in u:
+            u.append(x)
+    print(u)
+    count=[0]*6
+    for i in range(0,len(arr)):
+        count[arr[i]]+=1
+    print(count)
+    
+    return count.index(max(count))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
